fc144df058181ddf547906f7dbe5336c91cd3477
EC2 Default User <ec2-user@ip-172-26-11-114.eu-west-2.compute.internal>
2023-09-27 14:07:07 +0000

    Update test.java at 2023-09-27 14:07:07.534441 through application.

--- 1/test.java
+++ 1/test.java
@@ -12,7 +12,11 @@ class test {
     
     for (int i = 0; i < 5; i++) {
          System.out.println(i);
-         Thread.sleep(2000);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
   }
 }
